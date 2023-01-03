d7cbbfc1da3fbdb3a60ca83d5bab54725e5ec2d9
mrwolfovetz <121901386+mrwolfovetz@users.noreply.github.com>
2023-01-04 02:43:40 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,6 @@
+canvas = document.getElementById("gamescreen" );
+canvas.width = document.body.clientWidth; 
+canvas.height = document.body.clientHeight;
+ctx = canvas.getContext("2d");
+ctx.fillRect(50, 50, 100, 100);
+console.log(canvas.height)
