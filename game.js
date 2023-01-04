c512f626861df8f9d7e6c1c2c0a9d33c6dfd740d
mrwolfovetz <121901386+mrwolfovetz@users.noreply.github.com>
2023-01-04 19:56:17 +0300

    update

--- game.js
+++ game.js
@@ -2,4 +2,3 @@ canvas = document.getElementById("gamescreen" );
 canvas.width = document.body.clientWidth; 
 canvas.height = document.body.clientHeight;
 ctx = canvas.getContext("2d");
-ctx.fillRect(50, 50, 100, 100);
